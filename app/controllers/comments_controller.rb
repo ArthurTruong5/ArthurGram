6d05ed8b01082d4639354d97279731b9827fad7a
arthurtruong <arthurtruong.work@gmail.com>
2018-06-11 15:57:15 +1000

    added css for comments and delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 before_action :set_post
 
     def create
-      @comment = @post.comments.build(comment_params)
+      @comment = @post.comments.new(comment_params)
       @comment.user_id = current_user.id
 
 
