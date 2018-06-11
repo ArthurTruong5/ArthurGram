ac36247376b80a6981fbe3b2fedf10117ba9aad2
arthurtruong <arthurtruong.work@gmail.com>
2018-06-11 11:08:34 +1000

    added comment sections and css comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => { registrations: 'registrations' }
-    resources :posts
+    resources :posts do
+      resources :comments
+    end
 
     root 'posts#index', as: 'post/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
